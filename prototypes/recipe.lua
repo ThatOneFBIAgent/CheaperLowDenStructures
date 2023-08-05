78286f2aa0e2d29f2530eefbd7bd32783066940e
ThatOneFBIAgent <93457345+ThatOneFBIAgent@users.noreply.github.com>
2023-08-05 15:17:29 -0600

    Create recipe.lua and prototypes folder

---
+++ prototypes/recipe.lua
@@ -0,0 +1,19 @@
+data:extend(
+  {
+    
+    type = "recipe",
+    name = "low-density-structure",
+    {
+      enabled = false,
+      energy_required = settings.startup["cheaper-lds-time-requirement"].value,
+      ingredients =
+      {
+        {"steel-plate", settings.startup["cheaper-lds-steel-cost"].value},
+        {"plastic-bar", settings.startup["cheaper-lds-plastic-cost"].value},
+        {"copper-plate", settings.startup["cheaper-lds-copper-cost"].value}
+      },
+      result = "low-density-structure",
+	  result_count = 1
+    },
+   },
+)
