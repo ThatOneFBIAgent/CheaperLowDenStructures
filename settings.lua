8d1f8a58a9c8a389c853916634714fddea015522
ThatOneFBIAgent <93457345+ThatOneFBIAgent@users.noreply.github.com>
2023-08-05 15:10:26 -0600

    Uploaded files
    Initial upload

---
+++ settings.lua
@@ -0,0 +1,36 @@
+data:extend(
+    {
+    {
+      type = "int-setting",
+      name = "cheaper-lds-copper-cost",
+      setting_type = "startup",
+      minimum_value = 1,
+      default_value = 20
+      maximum_value = 100
+    },
+    {
+        type = "int-setting",
+        name = "cheaper-lds-plastic-cost",
+        setting_type = "startup",
+        minimum_value = 1,
+        default_value = 5
+        maximum_value = 100
+    },
+    {
+        type = "int-setting",
+        name = "cheaper-lds-steel-cost",
+        setting_type = "startup",
+        minimum_value = 1,
+        default_value = 2
+        maximum_value = 100
+    },
+    {
+        type = "int-setting",
+        name = "cheaper-lds-time-requirement",
+        setting_type = "startup",
+        minimum_value = 1,
+        default_value = 20
+        maximum_value = 200
+    }
+  }
+)
