3f4bdf1e61aceaece51ff5ff5a94126ebed7e948
ThatOneFBIAgent <93457345+ThatOneFBIAgent@users.noreply.github.com>
2023-08-05 18:47:54 -0600

    Update recipe.lua
    Added expensive mode and fixed startup issues

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -1,8 +1,8 @@
-data:extend(
+data:extend({
   {
-    
     type = "recipe",
     name = "low-density-structure",
+    normal =
     {
       enabled = false,
       energy_required = settings.startup["cheaper-lds-time-requirement"].value,
@@ -15,5 +15,18 @@ data:extend(
       result = "low-density-structure",
 	  result_count = 1
     },
-   },
-)
+    expensive =
+    {
+      enabled = false,
+      energy_required = settings.startup["cheaper-lds-time-requirement"].value,
+      ingredients =
+      {
+        {"steel-plate", settings.startup["cheaper-lds-steel-cost"].value},
+        {"plastic-bar", settings.startup["cheaper-lds-plastic-cost"].value},
+        {"copper-plate", settings.startup["cheaper-lds-copper-cost"].value}
+      },
+      result = "low-density-structure",
+	  result_count = 1
+    }
+  }
+})
